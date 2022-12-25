ca1c02fe2f170a5ccd7dd4e5dd2e2e7bf11c47ca
ForKpiLabs <you@example.combasenko.nikita.r@gmail.com>
2022-12-25 16:04:08 +0200

    parser v.3.3 - Now you can collect n_posts from each channel( n_posts <= max posts from search from channel). Without connection to DB

--- parser.py
+++ parser.py
@@ -260,37 +260,50 @@ def parse_twitter(query):
 
     return response
 
-def parse_tg(query):
+def parse_tg(query, n_posts):
     response = {
         'query': query,
-        'message': []
+        'content': []
     }
 
-    tg_links = ["https://t.me/UAonlii"]  # collected from DB
+    tg_links = ["https://t.me/UAonlii", "https://t.me/u_now"] #collected from DB
     for channel_link in tg_links:
         URL = channel_link[0:12] + "/s/" + channel_link[13::] + "?q=" + query.replace(" ", "+")
         channel = requests.get(URL).text
         soup = BeautifulSoup(channel, 'lxml')
         tgpost = soup.find_all('div', class_='tgme_widget_message')
-        full_message = {}
 
+        info = {
+            "channel_link": channel_link,
+            "message": []
+        }
+        count = 0
         for content in tgpost:
-            full_message['channel_link'] = channel_link
-            full_message['text'] = content.find('div', class_='tgme_widget_message_text').text
-            full_message['views'] = content.find('span', class_='tgme_widget_message_views').text
-            full_message['timestamp'] = content.find('time', class_='time').text
+            if count < n_posts:
+                full_message = {}
+                full_message['text'] = content.find('div', class_='tgme_widget_message_text').text
+                full_message['views'] = content.find('span', class_='tgme_widget_message_views').text
+                full_message['timestamp'] = content.find('time', class_='time').text
 
-            if content.find('a', class_='tgme_widget_message_photo_wrap') != None:
-                link = str(content.find('a', class_='tgme_widget_message_photo_wrap'))
-                full_message['url_image'] = re.findall(r"https://cdn4.*.*.jpg", link)[0]
-            elif 'url_image' in full_message:
-                full_message.pop('url_image')
-
-            response['message'].append(full_message)
-
+                if content.find('a', class_='tgme_widget_message_photo_wrap') != None:
+                    link = str(content.find('a', class_='tgme_widget_message_photo_wrap'))
+                    full_message['url_image'] = re.findall(r"https://cdn4.*.*.jpg", link)[0]
+                elif 'url_image' in full_message:
+                    full_message.pop('url_image')
+                info['message'].append(full_message)
+                count += 1
+            else:
+                break
+        response['content'].append(info)
+    # a = response['content'][0]
+    # print(a)
+    # print(len(a['message']))
+    # b = response['content'][1]
+    # print(b)
+    # print(len(b['message']))
     return response
 
 
 google_result = parse_google(query)
 twitter_result = parse_twitter(query)
-tg_result = parse_tg(query)
+tg_result = parse_tg(query, n_posts)
